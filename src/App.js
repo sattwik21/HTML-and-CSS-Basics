da02214050e0e59e125726d533e6c4f8202ad90f
Rohan Kumar Kar <77396533+rohankumarkar@users.noreply.github.com>
2022-10-13 21:37:49 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Navbar from './componenets/Navbar';
+import Home from './componenets/Home';
+import About from './componenets/About';
+import NoteState from './context/noteState';
+import Alert from './componenets/Alert';
+import Login from './componenets/Login';
+import { Signup } from './componenets/Signup';
+import { useState } from 'react';
+
+function App() {
+  const [alert, setAlert] = useState(null)
+  const showAlert = (message, type)=>{
+    setAlert({
+      msg:message,
+      type:type
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
+
+  return (
+    <>
+      <NoteState>
+        <BrowserRouter>
+          <Navbar />
+          <Alert alert= {alert}/>
+          <div className="container">
+            <Routes>
+              <Route exact path="/" element={<Home showAlert={showAlert} />} />
+              <Route exact path="/about" element={<About />} />
+              <Route exact path="/login" element={<Login showAlert={showAlert} />} />
+              <Route exact path="/signup" element={<Signup showAlert={showAlert} />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </NoteState>
+    </>
+  );
+}
+export default App;
