adf4c9c8e4977f6a331c4c266dbe37b914e84d2d
Rohan KumarKar <8001048@kfintech.com>
2022-10-17 14:37:10 +0530

    social media app

---
+++ SocialmediaApp-master/backend/middlewares/sendEmail.js
@@ -0,0 +1,21 @@
+const nodeMailer = require("nodemailer");
+
+exports.sendEmail = async (options) => {
+  const transporter = nodeMailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "f01db27c7211f0",
+      pass: "1f6200e208e32a",
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.SMPT_MAIL,
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+  };
+
+  await transporter.sendMail(mailOptions);
+};
